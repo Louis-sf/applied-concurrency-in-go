7d429630fb09fd7d576f1c8766a486251fbaea32
Louis-sf <shufanwl@gmail.com>
2023-05-28 16:56:40 -0700

    waitgroup

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
+	/*all types in the sync package should
+	be passed by pointer to functions*/
+	"sync"
 )
 
 func main() {
-	go hello()
-	time.Sleep(1 * time.Second)
+	var wg sync.WaitGroup
+	//add 1 because we only wait for 1 goroutine (hello)
+	wg.Add(1)
+	//pass by reference to avoid copying waitgroup instance
+	go hello(&wg)
+	wg.Wait()
 	goodbye()
 }
 
-func hello() {
+func hello(wg *sync.WaitGroup) {
+	//good practice to defered Done at the begining of the task
+	defer wg.Done()
 	fmt.Println("Hello, world!")
 }
 
